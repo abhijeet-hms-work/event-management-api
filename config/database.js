63c968b7386693ef4ff500b52d0521fc17ecdfa4
abhijeet-hms-work <abhijeet.hms.work@gmail.com>
2025-10-15 18:53:20 +0530

    Configure PostgreSQL connection pooling

---
+++ config/database.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    max: 20, // Maximum number of clients in the pool
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+});
+
+// Test database connection
+pool.on('connect', () => {
+    console.log('✓ Database connected successfully');
+});
+
+pool.on('error', (err) => {
+    console.error('Unexpected database error:', err);
+    process.exit(-1);
+});
+
+module.exports = pool;
